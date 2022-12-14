ac8dfb5093ffb4d8f5c46f9030bfe70c48e126d9
adimurdayani <adimurdayani@gmail.com>
2022-12-14 19:17:54 +0800

    *update

--- application/modules/home/views/index.php
+++ application/modules/home/views/index.php
@@ -221,7 +221,7 @@
 
 
 <!-- Team Start -->
-<div class="container-xxl py-5">
+<!-- <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
             <h6 class="text-primary">Guru</h6>
@@ -250,5 +250,5 @@
 
         </div>
     </div>
-</div>
+</div> -->
 <!-- Team End -->
--- application/modules/home/views/profil.php
+++ application/modules/home/views/profil.php
@@ -46,7 +46,7 @@
 
 
 <!-- Team Start -->
-<div class="container-xxl py-5">
+<!-- <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
             <h6 class="text-primary">Guru</h6>
@@ -73,5 +73,5 @@
             <?php endforeach; ?>
         </div>
     </div>
-</div>
+</div> -->
 <!-- Team End -->
